c5992742dfaab2a2e19301cfd52d884b3716cb33
zahramaura <zhrmauraa05@gmail.com>
2024-12-24 04:39:23 +0000

    pertemuan 35

--- app/Http/Controllers/dashboardcontroller.php
+++ app/Http/Controllers/dashboardcontroller.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\barangKeluar;
+use App\Models\pelanggan;
+use App\Models\stok;
+use App\Models\suplier;
 use Illuminate\Http\Request;
 
 class dashboardcontroller extends Controller
 {
     public function index(){
-        return view(
-            'dashboard.dashboard'
-        );
+        $getsuplier = suplier::count();
+        $getpelanggan = pelanggan::count();
+        $getSTOK = stok::count();
+        $getPendapatan = barangKeluar::sum('sub_total');
+        return view( 'dashboard.dashboard',compact(
+            'getSuplier',
+            'getPelanggan',
+            'getStok',
+            'getPendapatan',
+        ));
     }
 }
